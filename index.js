4f0f526a0f3bf1df404813653c6b1deb30d71932
ivancolmenares <ivancolmenares@live.com>
2021-11-03 08:58:27 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
 
-function ternaryCheckCity(){
-  // Write your code here!
+function scuberGreetingForFeet(depth){
+  if (depth <= 400) 
+  {  return 'This one is on me!';}
+   else if(depth <= 2500)
+   {  return 'I will gladly take your thirty bucks.';}
+   else (depth >= 2501)
+  { return 'No can do.'} 
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(place){
+{return ternaryCheckCity=place==="NYC" ? "Ok, sounds good.":"No go."
+}
+}
+function switchOnCharmFromTip(amount){
+switch (amount){
+  case 'generous':
+    return "Thank you so much.";
+    case 'not as generous':
+      return "Thank you.";
+      default:
+      return "Bye.";
+      
+}
 }
